de1a3086b8a0e434ef37d073960372b652f212da
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2023-02-04 23:10:31 +0530

    Skills completed

--- src/scenes/Profile.jsx
+++ src/scenes/Profile.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 function Profile() {
   return (
-    <div className="h-96">
-      <div className="flex justify-between p-10">
+    <div className="h-screen" id="profile">
+      <div className="flex justify-center items-center p-10">
         <div>
           <div className="text-4xl">Hi, I'm Arya Kulkarni</div>
           <div className="text-xl">a Full Stack Developer</div>
           <div className="text-lg">B.E. Somputer Science Engineering</div>
           <div className="text-md">(Expected Graduation: May 2004)</div>
         </div>
-        <div>Image</div>
       </div>
     </div>
   );
